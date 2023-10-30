3280c22602d6378924b224f3a84eeb51ecdab8cc
Jefferson Santos <Jeffersonnascto19@gmail.com>
2023-10-30 16:44:47 -0300

    Gerenciar Lance e anuncio.aspx.cs

--- TCC_euquero/Logica/GerenciarLance.cs
+++ TCC_euquero/Logica/GerenciarLance.cs
@@ -1,7 +1,10 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using TCC_euquero.Modelo;
 
@@ -9,6 +12,8 @@ namespace TCC_euquero.Logica
 {
     public class GerenciarLance : Banco
     {
+        List<Parametro> parametros = new List<Parametro>();
+
         public decimal ConsultarSaldo(string pEmail)
         {
             List<Parametro> lista = new List<Parametro>();
@@ -26,14 +31,46 @@ namespace TCC_euquero.Logica
             return saldo;
         }
 
-        public void DarLance(int pAnuncio, string pEmail, decimal pValor)
+        public void DarLance(int pAnuncio, string pEmailGanhadorNovo, decimal pValorAnterior, decimal pValorNovo)
         {
             List<Parametro> lista = new List<Parametro>();
             lista.Add(new Parametro("pAnuncio", pAnuncio.ToString()));
-            lista.Add(new Parametro("pEmail", pEmail));
-            lista.Add(new Parametro("pValor", pValor.ToString().Replace(',','.')));
+            lista.Add(new Parametro("pEmail", pEmailGanhadorNovo));
+            lista.Add(new Parametro("pValor", pValorNovo.ToString()));
 
             ExecutarProcedure("DarLance", lista);
+            EmailRessarcirSaldo(pAnuncio, pValorAnterior);
+        }
+
+        public void EmailRessarcirSaldo(int pAnuncio, decimal pValor)
+        {
+            string emailUsuario = "";
+            MySqlDataReader dados = ConsultarComando($"select nm_email_usuario_cliente as EmailUsuarioCliente from lance where cd_anuncio = {pAnuncio} and vl_lance = {pValor.ToString().Replace(',', '.')};");
+
+            if (dados.Read())
+                emailUsuario = dados["EmailUsuarioCliente"].ToString();
+            
+            dados.Close();
+            
+            MailMessage mail = new MailMessage("leilaoEuQuero@outlook.pt", emailUsuario);
+            EnvioDeEmail envioDeEmail = new EnvioDeEmail();
+
+            string CSS = "";
+
+            mail.Subject = $"Saldo Ressarcido!";
+            mail.IsBodyHtml = true;
+            mail.Body = $@"     <html>
+                                    <body>
+                                        <div style={CSS}>
+                                            <p> O valor de {pValor.ToString("C", new CultureInfo("pt-br"))} foi ressarcido para você.</p>
+                                        </div>
+                                    </body>
+                                </html>";
+
+            mail.SubjectEncoding = Encoding.GetEncoding("UTF-8");
+            mail.BodyEncoding = Encoding.GetEncoding("UTF-8");
+
+            envioDeEmail.Enviar(mail);
         }
     }
 }
--- TCC_euquero/anuncio.aspx.cs
+++ TCC_euquero/anuncio.aspx.cs
@@ -18,7 +18,9 @@ namespace TCC_euquero
             if (Session["email"] != null)
             {
                 GerenciarLance gerenciar = new GerenciarLance();
+                litSaldoUsuario.Text = "Seu saldo é de: " + gerenciar.ConsultarSaldo(Session["email"].ToString()).ToString();
 
+                txtLance.Enabled = true;
             }
 
             if (String.IsNullOrEmpty(Request["codProduto"]))
@@ -46,9 +48,7 @@ namespace TCC_euquero
             litLances.Text = anuncio.QntLances.ToString();
             litLeiloeiro.Text = anuncio.NomeUsuario;
             litGanhador.Text = anuncio.GanhadorAtual;
-
-            litLanceAtual.Text = anuncio.LanceAtual.ToString("C", new CultureInfo("pt-br"));
-
+            litLanceAtual.Text = anuncio.LanceAtual.ToString();
             litValorInicial.Text = anuncio.ValorMinimo.ToString("C", new CultureInfo("pt-br"));
 
 
@@ -135,7 +135,7 @@ namespace TCC_euquero
                 {
                     valorLance = decimal.Parse(txtLance.Text);
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
                     throw new Exception("Não foi possível converter o valor da caixa de texto em decimal.");
                 }
@@ -144,24 +144,24 @@ namespace TCC_euquero
 
                 decimal saldo = gerenciarLance.ConsultarSaldo(email);
                 
-                //litRespostaSistema.Text = "-";
+                litRespostaSistema.Text = "-";
 
                 if (saldo >= valorLance)
                 {
-                    gerenciarLance.DarLance(cdAnuncio, email, valorLance);
+                    gerenciarLance.DarLance(cdAnuncio, email, decimal.Parse(litLanceAtual.Text), valorLance);
 
                     //Dar Refresh na página
                     Response.Redirect(Page.Request.Url.ToString());
-                    //litRespostaSistema.Text = "Seu lance foi efetuado com sucesso!";
+                    litRespostaSistema.Text = "Seu lance foi efetuado com sucesso!";
                 }
                 else
                 {
-                    //litRespostaSistema.Text = "Saldo insuficiente!";
+                    litRespostaSistema.Text = "Saldo insuficiente!";
                 }
             }
             else
             {
-                //litRespostaSistema.Text = "Você não está cadastrado!";
+                litRespostaSistema.Text = "Você não está cadastrado!";
             }
         }
 
